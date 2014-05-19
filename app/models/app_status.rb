0e99dcb22b1af3d0004561b118e61144c7ddb699
Danny Whalen <daniel.r.whalen@gmail.com>
2014-05-19 12:09:05 -0700

    Namespace model classes

--- app/models/app_status.rb
+++ app/models/app_status.rb
@@ -1,25 +1,29 @@
-class AppStatus
-  OK = 'ok'.freeze
-  FAILURE = 'not ok'.freeze
+module Transitmix
+  module Models
+    class AppStatus
+      OK = 'ok'.freeze
+      FAILURE = 'not ok'.freeze
 
-  def payload
-    {
-      dependencies: %w(heroku-postgresql heroku-scheduler mapbox mapbox-smart-directions),
-      status: status,
-      updated: Time.now.to_i,
-      resources: {}
-    }
-  end
+      def payload
+        {
+          dependencies: %w(heroku-postgresql heroku-scheduler mapbox mapbox-smart-directions),
+          status: status,
+          updated: Time.now.to_i,
+          resources: {}
+        }
+      end
 
-  def status
-    raise if DB.tables.empty?
+      def status
+        raise if Transitmix::App.database.tables.empty?
 
-    OK
-  rescue
-    FAILURE
-  end
+        OK
+      rescue
+        FAILURE
+      end
 
-  def to_json
-    JSON.pretty_generate(payload)
+      def to_json
+        JSON.pretty_generate(payload)
+      end
+    end
   end
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,25 +1,29 @@
-class Map < Sequel::Model
-  plugin :timestamps, update_on_create: true
-  plugin :json_serializer, :include=>:lines
-  plugin :serialization, :json, :center
+module Transitmix
+  module Models
+    class Map < Sequel::Model
+      plugin :timestamps, update_on_create: true
+      plugin :json_serializer, :include=>:lines
+      plugin :serialization, :json, :center
 
-  set_allowed_columns :name, :center, :zoom_level
+      set_allowed_columns :name, :center, :zoom_level
 
-  one_to_many :lines
+      one_to_many :lines
 
-  def remix
-    new_map = Map.new(allowed_attributes)
-    new_map.remixed_from_id = id
+      def remix
+        new_map = Map.new(allowed_attributes)
+        new_map.remixed_from_id = id
 
-    db.transaction do
-      new_map.save!
-      save!
+        db.transaction do
+          new_map.save!
+          save!
 
-      lines.each do |line|
-        new_map.add_line(line.allowed_attributes)
+          lines.each do |line|
+            new_map.add_line(line.allowed_attributes)
+          end
+        end
+
+        new_map
       end
     end
-
-    new_map
   end
 end
