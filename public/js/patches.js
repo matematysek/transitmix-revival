01de0fca8c10b4fcf936f54395b495a736513008
Sam Hashemi <sam@samhashemi.com>
2014-07-02 09:11:08 -0700

    Throw an error if we attempt to modify a different version of Leaflet.

--- public/js/patches.js
+++ public/js/patches.js
@@ -1,3 +1,8 @@
+// Make sure we're modifying the correct version of Leaflet
+if (L.version !== '0.7.2') {
+  throw new Error('Attempting to patch Leaflet ' + L.version + '. Only 0.7.2 is supported');
+}
+
 // Modify Draggable to ignore shift key. 
 L.Draggable.prototype._onDown = function(e) {
   this._moved = false;
